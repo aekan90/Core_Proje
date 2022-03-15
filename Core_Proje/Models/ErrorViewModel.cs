2ef2999ae17746c8f89de875bc9d7eb36e6dd817
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-03-15 10:32:18 +0300

    Başlangıç

--- Core_Proje/Models/ErrorViewModel.cs
+++ Core_Proje/Models/ErrorViewModel.cs
@@ -4,6 +4,7 @@ namespace Core_Proje.Models
 {
     public class ErrorViewModel
     {
+        // Hata View
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
