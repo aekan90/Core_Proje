62af7ef72a8a08c964b7f73c8cddb36d869d49fb
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-03-16 16:02:38 +0300

    D10 - KATMANLAR ARASI REFERANSLAR EKLENDİ

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    // Context Classı
+    // 1-) DB yapılandırmamız içindeki bağlantı stringini tutar.
+    // 2-) DB 'ye yansıtılacak olan tabloları tutar             
+    public class Context : DbContext // DB Setleri kullanabilmek için 
+    {
+        // tabloları property olarak çağıracağız.
+
+        // Bağlantı Stringi Ayarlıyoruz. Bizim bağlantı adresimizi tutacak
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("DESKTOP-OA1OLIM;database=CoreProjeDB;integrated security=true");
+        }
+
+        // Entity katmanındaki sınıfları tek tek burada çağırmalıyız.
+        public int MyProperty { get; set; }
+    }
+}
