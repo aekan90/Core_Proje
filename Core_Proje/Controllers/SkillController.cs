d435a4a38adafa8681341c31af4f703d4ee052d1
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-04 15:52:08 +0300

    AddSkill to Form

--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -11,6 +12,20 @@ namespace Core_Proje.Controllers
         {
             var values = skillManager.TGetList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSkill() // Yeni Yetenek Ekle - Get
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill) // Yeni Yetenek Ekle - Post
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
         }
     }
 }
