ea102d15b751b3858458c9b8b31ebf46dd391d1e
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-06 15:10:59 +0300

    Experience

--- Core_Proje/Controllers/ExperienceController.cs
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Core_Proje.Controllers
 {
@@ -14,6 +16,52 @@ namespace Core_Proje.Controllers
 
             var values = experienceManager.TGetList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddExperience() // Yeni Deneyim Ekle - Get
+        {
+            ViewBag.Url1 = "Deneyim Ekle";
+            ViewBag.Url2 = "Experience";
+            ViewBag.Url3 = "AddExperience";
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience) // Yeni Yetenek Ekle - Post
+        {
+            experienceManager.TAdd(experience);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet] // bunu yazmasan da olur varsayılanı HttpGet zaten, ama sen yaz
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = experienceManager.TGetById(id);
+            experienceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditExperience(int id) // Deneyim Güncelle - GET(urlden)
+        {
+            ViewBag.Url1 = "Deneyim Güncelleme";
+            ViewBag.Url2 = "Experience";
+            ViewBag.Url3 = "EditExperience";
+            var values = experienceManager.TGetById(id);
+            List<Experience> veri = new List<Experience>
+            {
+                values
+            };
+            return View(veri);
+        }
+
+        [HttpPost]
+        public IActionResult EditExperience(Experience experience) // Deneyim Güncelle - GET(urlden)
+        {
+            experienceManager.TUpdate(experience);
+            return RedirectToAction("Index");
         }
     }
 }
