7fc050ed88cb44dddaf29f1643f4b2b98f6bc427
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-06 10:04:28 +0300

    Deneyim Listeleme

---
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            ViewBag.Url1 = "Deneyimler";
+            ViewBag.Url2 = "Experience";
+
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+    }
+}
