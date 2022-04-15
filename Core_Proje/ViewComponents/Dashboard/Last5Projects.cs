38983612652b1a9106a25015cfc439274357f037
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-15 11:34:57 +0300

    projelerimden 5 tanesini dahsboard da listeledim

--- Core_Proje/ViewComponents/Dashboard/Last5Projects.cs
+++ Core_Proje/ViewComponents/Dashboard/Last5Projects.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.ViewComponents.Dashboard
 {
     public class Last5Projects : ViewComponent
     {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = portfolioManager.TGetList();
+            return View(values);
         }
     }
 }
