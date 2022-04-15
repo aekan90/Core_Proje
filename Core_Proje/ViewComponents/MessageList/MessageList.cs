b989492210e4a4fadeedc6a73959c441902b221e
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-15 15:22:22 +0300

    mesajlist viewcomponenti oluşturuldu

---
+++ Core_Proje/ViewComponents/MessageList/MessageList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.MessageList
+{
+    public class MessageList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
