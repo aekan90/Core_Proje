a61b1664865b4b14dac964e05c9e3df0cc635feb
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-19 13:19:57 +0300

    .

---
+++ BusinessLayer/Concrete/UserManager.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public void TAdd(User t)
+        {
+            _userDal.Insert(t); // GenericRepository<User>.Insert() 
+        }
+
+        public void TDelete(User t)
+        {
+            _userDal.Delete(t);
+        }
+
+        public User TGetById(int id)
+        {
+            return _userDal.GetById(id);
+        }
+
+        public List<User> TGetList()
+        {
+            return _userDal.GetList();
+        }
+
+        public void TUpdate(User t)
+        {
+            _userDal.Update(t);
+        }
+    }
+}
