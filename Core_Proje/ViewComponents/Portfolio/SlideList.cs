dca21016c0637298a9c35e4789f1b5a95f5c94f4
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-22 09:42:00 +0300

    Slide List Tamamlandı

---
+++ Core_Proje/ViewComponents/Portfolio/SlideList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Portfolio
+{
+    public class SlideList : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var AllPortfolioList = portfolioManager.TGetList();
+            return View(AllPortfolioList); // --> Views/Shared/SlideList/Default.cshtml ' de karşılanıyor. 
+        }
+    }
+}
