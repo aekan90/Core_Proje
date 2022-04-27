59efaddf9970b6da4a91bd37180b84a271f07987
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-27 13:04:40 +0300

    UserRegisterViewModel oluşturuldu

---
+++ Core_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_Proje.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {// AspNetUsers tablosundaki sütunlara karşılık gelen propertyler
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Giriniz")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Parola Giriniz")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Parolayı Tekrar Giriniz")]
+        [Compare("Password", ErrorMessage = "Şifreler Uyumlu Değil")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Eposta Giriniz")]
+        public string Email { get; set; }
+    }
+}
