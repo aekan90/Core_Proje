36453e99aa812eb594fa23ea980514ad59481ed8
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-08-23 09:57:22 +0300

    _signInManager

--- Core_Proje/Areas/Writer/Controllers/LoginController.cs
+++ Core_Proje/Areas/Writer/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,13 @@ namespace Core_Proje.Areas.Writer.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly SignInManager<WriterUser> _signInManager;
+
+        public LoginController(SignInManager<WriterUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
